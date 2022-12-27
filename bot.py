37b2073c99df57581d3f36d3a8a57815d50374a0
pascal <paschy2002@gmail.com>
2022-12-27 06:15:54 +0100

    first requests done

--- bot.py
+++ bot.py
@@ -2,15 +2,21 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+import pymongo
+from pymongo import MongoClient
 # Load environment variables from .env file
 load_dotenv()
 
 # Access environment variables
 discord_bot_key = os.getenv("DISCORD_BOT_KEY")
+mongo_db_key = os.getenv("MONGO_DB_KEY")
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
+client_mongo = MongoClient(
+    mongo_db_key)
+db = client_mongo["BlueLOCK"]
+users_collection = db["users"]
 
 
 @client.event
@@ -20,7 +26,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    user_id = message.author.id
     if message.content.startswith('hi'):
+        users_collection.update_one(
+            {"user_id": user_id},
+            {"$set": {"command_used": True}}
+        )
         await message.channel.send('Hello!')
 
 
