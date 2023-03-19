1324d1969f708b6a55b23f965bbe0b202d423a66
pascal <paschy2002@gmail.com>
2023-03-19 18:44:29 +0100

    added odds to daily pull

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 from discord import Interaction, app_commands, ui
 from discord.ext import commands, tasks
 from discord.utils import MISSING
+from discord.ui import Button, View
 
 load_dotenv()
 
@@ -15,11 +16,40 @@ mongo_db_key = os.getenv("MONGO_DB_KEY")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 client_mongo = MongoClient(
     mongo_db_key)
 db = client_mongo["BlueLOCK"]
 users_collection = db["users"]
+cards_collection = db["cards"]
+
+
+class MyView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.button = discord.ui.Button(
+            style=discord.ButtonStyle.green, label="Click me!")
+        self.add_item(self.button)
+
+    async def button_callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Button clicked!", ephemeral=True)
+
+
+def get_random_float():
+    return round(random.uniform(0.00, 100.00), 2)
+
+
+def get_card_rarity():
+    randFloat = get_random_float()
+    if randFloat <= 0.5:  # 0.5%
+        return "Legendary"
+    if randFloat <= 5.5:  # 5%
+        return "Epic"
+    if randFloat <= 45.5:  # 40%
+        return "Rare"
+    else:
+        return "Common"
 
 
 async def send_random_image(channel):
@@ -32,22 +62,40 @@ async def send_random_image(channel):
     return image_file
 
 
-@client.event
+async def send_random_card(channel):
+    card_name = roll_summon_category("common")
+    await channel.send(card_name)
+
+
+async def roll_summon_category(rarity):
+    cardsQuery = cards_collection.find({"rarity": rarity})
+    cards = list(cards_collection)
+    random_card = random.choice(cards)
+    return (random_card["name"])
+
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
 
-@client.command(description="Says hello")
+@bot.command()
+async def random_card(ctx):
+    send_random_card(ctx.channel)
+    await ctx.send("Testing s!ummon")
+
+
+@bot.command(description="Says hello")
 async def hello(ctx):
     await ctx.send('Hello, world!')
 
 
-@client.command()
+@bot.command()
 async def helpme(ctx):
     await ctx.send('Available commands:\n /register \n /card\n /inventory')
 
 
-@client.command()
+@bot.command()
 async def reset(ctx):
     users_collection.update_many(
         {},
@@ -56,7 +104,7 @@ async def reset(ctx):
     await ctx.send('Reset ! Everyone can summon now !')
 
 
-@client.command()
+@bot.command()
 async def register(ctx):
     user_id = ctx.author.id
     username = str(ctx.author)
@@ -69,7 +117,7 @@ async def register(ctx):
         await ctx.send('Already registered !')
 
 
-@client.command()
+@bot.command()
 async def summon(ctx):
     user_id = ctx.author.id
     username = str(ctx.author)
@@ -90,7 +138,7 @@ async def summon(ctx):
         await ctx.send('Command already used !')
 
 
-@client.command()
+@bot.command()
 async def inventory(ctx):
     user_id = ctx.author.id
     doc = users_collection.find_one({"user_id": user_id})
@@ -98,12 +146,18 @@ async def inventory(ctx):
         await ctx.send(file=discord.File("images/banner1/" + cards))
 
 
-@client.command()
+@bot.command()
 async def banner(ctx):
     image_files = os.listdir("images/banner1/")
     for cards in image_files:
         await ctx.send(file=discord.File("images/banner1/" + cards))
 
 
-client.run(
+@bot.command()
+async def send_button(ctx):
+    view = MyView()
+    await ctx.send("Testing buttons!", view=view)
+
+
+bot.run(
     discord_bot_key)
--- test2.py
+++ test2.py
@@ -1,30 +1,178 @@
-import discord
-from discord.ext import commands
+from typing import Optional
 
-from dotenv import load_dotenv
+import discord
 import os
+import random
+from dotenv import load_dotenv
+import pymongo
+from pymongo import MongoClient
+from discord import Interaction, app_commands, ui
+from discord.ext import commands, tasks
+
+from discord.ui import Button, View
 load_dotenv()
-intents = discord.Intents.default()
-intents.message_content = True
 
 discord_bot_key = os.getenv("DISCORD_BOT_KEY")
+mongo_db_key = os.getenv("MONGO_DB_KEY")
+guild_id = os.getenv("GUILD_ID")
+client_mongo = MongoClient(
+    mongo_db_key)
+db = client_mongo["BlueLOCK"]
+users_collection = db["users"]
+cards_collection = db["cards"]
+MY_GUILD = discord.Object(id=guild_id)  # replace with your guild id
 
-# Define the bot client
-bot = commands.Bot(command_prefix='!', intents=intents)
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
 
 
-@bot.event
+class MyView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        self.button = discord.ui.Button(
+            style=discord.ButtonStyle.green, label="Click me!")
+        self.add_item(self.button)
+
+    async def button_callback(self, interaction: discord.Interaction):
+        await interaction.channel.send('Button clicked!', ephemeral=True)
+
+
+def get_random_float():
+    return round(random.uniform(0.00, 100.00), 2)
+
+
+def get_card_rarity():
+    randFloat = get_random_float()
+    if randFloat <= 0.5:  # 0.5%
+        print("legendary")
+        return "legendary"
+    if randFloat <= 5.5:  # 5%
+        print("epic")
+        return "epic"
+    if randFloat <= 45.5:  # 40%
+        print("rare")
+        return "rare"
+    else:
+        print("common")
+        return "common"
+
+
+async def send_random_card(channel):
+    card_name = roll_summon_category("common")
+    await channel.send_message(card_name)
+
+
+async def roll_summon_category(rarity):
+    cardsQuery = cards_collection.find({"rarity": rarity})
+    cards = list(cardsQuery)
+    random_card = random.choice(cards)
+    return (random_card["name"])
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+
+@client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
 
 
-# Define a command
+@client.tree.command()
+async def random_card(interaction: discord.Interaction):
+    card = await roll_summon_category(get_card_rarity())
+    await interaction.response.send_message(card)
 
 
-@bot.command()
-async def hello(ctx):
-    print("hello() function called")
-    await ctx.send("Hello!")
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    channel = interaction.channel
+    await channel.send("Hello, this is a message in a channel!")
 
-# Run the bot
-bot.run(discord_bot_key)
+
+@client.tree.command()
+async def reset(interaction: discord.Interaction):
+    """Resets everyone's summons !"""
+    users_collection.update_many(
+        {},
+        {"$set": {"command_used": False}}
+    )
+    await interaction.response.send_message('Reset ! Everyone can summon now !')
+
+
+@client.tree.command()
+async def register(interaction: discord.Interaction):
+    """Start collecting cards right now !"""
+    doc = users_collection.find_one({"user_id": interaction.user.id})
+    if doc is None:
+        users_collection.insert_one(
+            {"user_id": interaction.user.id, "username": interaction.user, "command_used": False, })
+        await interaction.response.send_message('Register complete ! Have fun :)')
+    else:
+        await interaction.response.send_message('Already registered !')
+
+
+@client.tree.command()
+async def daily(interaction: discord.Interaction):
+    """Daily summon !"""
+
+    doc = users_collection.find_one({"user_id": interaction.user.id})
+    if doc is None:
+        await interaction.response.send_message('User not registered ! Use /register command')
+    elif (doc['command_used'] == False):
+        image_file = await send_random_image(interaction.response)
+        users_collection.update_one(
+            {"user_id": interaction.user.id},
+            {"$set": {"command_used": True}}
+        )
+        users_collection.update_one(
+            {"user_id": interaction.user.id},
+            {"$push": {"dropped_images": image_file}}
+        )
+    else:
+        await interaction.response.send_message('Command already used !')
+
+
+@client.tree.command()
+async def inventory(interaction: discord.Interaction):
+    """Shows your player inventory!"""
+    doc = users_collection.find_one({"user_id": interaction.user.id})
+
+    embed = discord.Embed()
+    for card in doc["dropped_images"]:
+        embed.set_image(url="attachment://" + card)
+        await interaction.channel.send(embed=embed, file=discord.File("images/banner1/" + card))
+
+
+@client.tree.command()
+async def embed(interaction: discord.Interaction):
+    """embedMultipleImages test 4 imgs"""
+    embed1 = discord.Embed().set_image(url="https://i.imgur.com/sCcejmy.png")
+    embed2 = discord.Embed().set_image(url="https://i.imgur.com/TddUQWs.png")
+
+    await interaction.response.send_message(embeds=[embed1, embed2])
+
+
+client.run(
+    discord_bot_key)
