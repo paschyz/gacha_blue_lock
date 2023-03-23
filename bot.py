0b90fa27ea5507386716e8f5ce657a544cacee66
pascal <paschy2002@gmail.com>
2023-03-23 02:29:09 +0100

    added carousel for banner command and inventory, cleaned up code overrall and added a role restriction on the reset command

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import random
 from dotenv import load_dotenv
 import pymongo
 from pymongo import MongoClient
-from discord import Interaction, app_commands, ui
+from discord import Interaction, app_commands, ui, ButtonStyle
 from discord.ext import commands, tasks
 
 from discord.ui import Button, View
@@ -26,28 +26,28 @@ MY_GUILD = discord.Object(id=guild_id)  # replace with your guild id
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
-        # A CommandTree is a special type that holds all the application command
-        # state required to make it work. This is a separate class because it
-        # allows all the extra state to be opt-in.
-        # Whenever you want to work with application commands, your tree is used
-        # to store and work with them.
-        # Note: When using commands.Bot instead of discord.Client, the bot will
-        # maintain its own tree instead.
         self.tree = app_commands.CommandTree(self)
 
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
 
-class MyView(View):
-    @discord.ui.button(label="Click me")
-    async def on_button_click(self, button: Button, interaction: Interaction):
-        await interaction.response.send_message("Button clicked!")
+class Carousel(View):
+    def __init__(self, items):
+        super().__init__()
+        self.items = items
+        self.current_index = 0
+
+    @discord.ui.button(emoji="⬅️")
+    async def button_left(self, interaction: Interaction, button: Button):
+        self.current_index = (self.current_index - 1) % len(self.items)
+        await interaction.response.edit_message(embed=self.items[self.current_index])
+
+    @discord.ui.button(emoji="➡️")
+    async def button_right(self, interaction: Interaction, button: Button):
+        self.current_index = (self.current_index + 1) % len(self.items)
+        await interaction.response.edit_message(embed=self.items[self.current_index])
 
 
 def get_random_float():
@@ -100,12 +100,14 @@ async def roll_summon_category(rarity):
 def get_color(rarity):
     if rarity == "legendary":
         return discord.Colour.gold()
-    if rarity == "epic":
+    elif rarity == "epic":
         return discord.Colour.purple()
-    if rarity == "rare":
+    elif rarity == "rare":
         return discord.Colour.blue()
-    if rarity == "common":
+    elif rarity == "common":
         return discord.Colour.green()
+    else:
+        return discord.Colour.default()
 
 
 intents = discord.Intents.default()
@@ -118,29 +120,22 @@ async def on_ready():
 
 
 @client.tree.command()
-async def button_test(interaction: discord.Interaction):
-    """Test a button"""
-    view = MyView()
-    # @client.tree.command()
-    await interaction.response.send_message("Testing a button!", view=view)
-
-
-@client.tree.command()
 async def reset(interaction: discord.Interaction):
-    """Resets everyone's summons !"""
-    if (interaction.user.id == 314809676447350785):
+    """Resets everyone's daily summon ! Only available for the admins"""
+    required_role = discord.utils.get(interaction.guild.roles, name='Admin')
+    if required_role in interaction.user.roles:
         users_collection.update_many(
             {},
             {"$set": {"command_used": False}}
         )
         await interaction.response.send_message('Reset ! Everyone can summon now !')
     else:
-        await interaction.response.send_message('You are not authorized to use this command !')
+        await interaction.response.send_message('You are not an admin ! You are not authorized to use this command !')
 
 
 @client.tree.command()
 async def register(interaction: discord.Interaction):
-    """Start collecting cards right now !"""
+    """Conquer this world with your ego !"""
     doc = users_collection.find_one({"user_id": interaction.user.id})
     if doc is None:
         user_id = interaction.user.id
@@ -154,7 +149,7 @@ async def register(interaction: discord.Interaction):
 
 @client.tree.command()
 async def daily(interaction: discord.Interaction):
-    """Daily summon ! \n Common (54.5%) \n Rare (40%) \n Epic (5%) \n Legendary (0.5%)"""
+    """Daily summon !   Common (54.5%)   Rare (40%)   Epic (5%)   Legendary (0.5%)"""
 
     doc = users_collection.find_one({"user_id": interaction.user.id})
     if doc is None:
@@ -183,23 +178,26 @@ async def daily(interaction: discord.Interaction):
 
 
 @client.tree.command()
-async def inventory(interaction: discord.Interaction):
-    """Shows your inventory!"""
+async def inventory(interaction: discord.Integration):
+    """Shows your inventory !"""
     if (verify_if_user_exists(interaction.user.id)):
         if (verify_user_inventory(interaction.user.id)):
+            items = []
             doc = users_collection.find_one({"user_id": interaction.user.id})
-            embed = discord.Embed()
+
             for card in doc['dropped_images']:
-                embed.title = interaction.user.name + "'s inventory"
                 url_card = cards_collection.find_one({"name": card})[
                     "card_image"]
                 color = get_color(cards_collection.find_one(
                     {"name": card})["rarity"])
-                rarity = cards_collection.find_one({"name": card})["rarity"]
+                rarity = cards_collection.find_one({"name": card})["name"]
+                embed = discord.Embed(title=interaction.user.name + "'s inventory",
+                                      description=rarity.capitalize(), color=color,
+                                      url=url_card)
                 embed.set_image(url=url_card)
-                embed.description = rarity.capitalize()
-                embed.colour = color
-                await interaction.channel.send(embed=embed)
+                items.append(embed)
+            carousel = Carousel(items)
+            await interaction.response.send_message(embed=embed, view=carousel)
         else:
             await interaction.response.send_message('No cards in inventory !')
     else:
@@ -208,7 +206,7 @@ async def inventory(interaction: discord.Interaction):
 
 @client.tree.command()
 async def reroll(interaction: discord.Interaction):
-    """Reroll all your summons !"""
+    """Reroll your account !    WARNING : This will delete your inventory and all you ressources !"""
     if (verify_if_user_exists(interaction.user.id)):
         users_collection.update_one(
             {"user_id": interaction.user.id}, {"$set": {"command_used": False}})
@@ -221,7 +219,7 @@ async def reroll(interaction: discord.Interaction):
 
 @client.tree.command()
 async def multi(interaction: discord.Interaction):
-    """Multi-summon right now !"""
+    """Summon 3 cards ! Only available for new users !"""
     if (verify_if_user_exists(interaction.user.id)):
         if (verify_user_inventory(interaction.user.id)):
             await interaction.response.send_message('You already have cards in your inventory ! Use /reroll command to reroll your summons')
@@ -244,23 +242,18 @@ async def multi(interaction: discord.Interaction):
 
 @client.tree.command()
 async def banner(interaction: discord.Interaction):
-    """Show banner !"""
+    """Show current banner !"""
     doc = cards_collection.find()
+    embeds = []
+
     for card in doc:
         embed = discord.Embed()
         embed.title = card["name"].capitalize()
         embed.set_image(url=card["card_image"])
         embed.description = card["rarity"].capitalize()
         embed.colour = get_color(card["rarity"])
-        await interaction.channel.send(embed=embed)
-
-# @client.tree.command()
-# async def embed(interaction: discord.Interaction):
-#     """embedMultipleImages test 4 imgs"""
-#     embed1 = discord.Embed().set_image(url="https://i.imgur.com/sCcejmy.png")
-#     embed2 = discord.Embed().set_image(url="https://i.imgur.com/TddUQWs.png")
-
-#     await interaction.response.send_message(embeds=[embed1, embed2])
+        embeds.append(embed)
+    await interaction.channel.send(embed=embed, view=Carousel(embeds))
 
 
 client.run(
